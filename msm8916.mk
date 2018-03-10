a40f3a8c02559bf8ff4b2687f15a972c16299bab
DroidFreak32 <rushabshah32@gmail.com>
2018-03-11 00:37:28 +0530

    msm8916-common: Drop audiod
    * It's now deprecated
    
    Change-Id: I00c8bdc09da4ba39508314ec8feba67d60e4fb04

--- msm8916.mk
+++ msm8916.mk
@@ -58,7 +58,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    audiod \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
