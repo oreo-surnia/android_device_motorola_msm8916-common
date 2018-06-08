Harpia-development/device_motorola_msm8916-common

CameraWrapper: update library dependencies for O

camera-wrapper/Android.mk
@@ -1,0 +1,28 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES := \
+    framework/native/include \
+    system/media/camera/include
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware \
+    liblog \
+    libcamera_client \
+    libgui \
+    libhidltransport \
+    libsensor \
+    libutils \
+    android.hidl.token@1.0-utils
+
+LOCAL_STATIC_LIBRARIES := \
+    libarect
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)

oreo-surnia/android_device_motorola_msm8916-common

camera-wrapper/Android.mk
@@ -2,14 +2,25 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := \
+    frameworks/native/include \
+    frameworks/native/include/media/openmax \
     system/media/camera/include \
-    frameworks/native/include/media/openmax
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libgui
+    android.hidl.token@1.0-utils \
+    libcamera_client \
+    libgui \
+    libhardware \
+    libhidltransport \
+    liblog \
+    libsensor \
+    libutils
+
+LOCAL_STATIC_LIBRARIES := \
+    libarect
 
 ifneq ($(filter harpia lux, $(TARGET_DEVICE)),)
 LOCAL_CFLAGS += -DCLOSE_NATIVE_HANDLE